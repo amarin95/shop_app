bf8dbf6eabc91a61fda9dba3911e072f2033fc4a
Alberto Marín González <alberto.marin@imatiasl.lan>
2020-10-26 16:20:50 +0100

    Added edit product screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 import 'package:flutter_complete_guide/widgets/app_drawer.dart';
 import 'package:provider/provider.dart';
 
@@ -16,7 +17,9 @@ class UserProductsScreen extends StatelessWidget {
           actions: [
             IconButton(
               icon: const Icon(Icons.add),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              },
             ),
           ],
         ),
