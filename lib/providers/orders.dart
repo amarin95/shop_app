a383d737a2f3c130db46417848b7d42eecf14575
Alberto Marín González <alberto.marin@imatiasl.lan>
2020-11-17 11:04:39 +0100

    fetch and set orders added

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -20,6 +20,23 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final url = 'https://flutter-test-9731d.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) return;
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['DateTime']),
+          products: (orderData['products'] as List<dynamic>).map((item) => CartItem(id: item['id'], price: item['price'], quantity: item['quantity'], title: item['title'])).toList()));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future addOrder(List<CartItem> cartProducts, double total) async {
     final url = 'https://flutter-test-9731d.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,18 +5,53 @@ import 'package:provider/provider.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart' as oi;
 
-
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+Future _ordersFuture;
+
+Future _obtainOrdersFuture(){
+  return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+}
+
+  //to avoid rerun of the http request if rebuild => 
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    //final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(itemBuilder: (ctx, i) =>  oi.OrderItem(orderData.orders[i]), itemCount: orderData.orders.length)
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              //error handling
+              return Center(
+                child: Text('Error has ocurred!'),
+              );
+            } else {
+              return Consumer<Orders>(builder: (ctx, orderData, child) => ListView.builder(itemBuilder: (ctx, i) => oi.OrderItem(orderData.orders[i]), itemCount: orderData.orders.length));
+            }
+          }
+        },
+      ),
     );
   }
 }
