bd1beb4e940d7254f8dd13784988d45830c8577e
Alberto Marin <alberx1965@gmail.com>
2021-01-18 18:44:31 +0100

    added signIn and login methods

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import 'providers/auth.dart';
 import 'providers/orders.dart';
 import './screens/user_products_screen.dart';
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
@@ -41,7 +45,7 @@ class MyApp extends StatelessWidget {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
-            UserProductsScreen.routeName : (ctx) => UserProductsScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
             EditProductScreen.routeName: (ctx) => EditProductScreen(),
           }),
     );
